0fa368e2f6c226fb0a73019f80fabf7059b8c575
Collins Kipkemoi <chemerilcollins@gmail.com>
2024-06-01 02:50:58 +0200

    Set up all the components, routers. Fix: Navbar collapse and expand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {MainComponent} from "./main/main.component";
+import {IssueListComponent} from "./issue-list/issue-list.component";
+import {DetailComponent} from "./detail/detail.component";
+import {NewIssueComponent} from "./new-issue/new-issue.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, MainComponent, IssueListComponent, DetailComponent, NewIssueComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'issue-tracker';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DetailComponent} from "./detail/detail.component";
+import {IssueListComponent} from "./issue-list/issue-list.component";
+import {NewIssueComponent} from "./new-issue/new-issue.component";
+import {MainComponent} from "./main/main.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path : "",
+    component : MainComponent
+  },
+  {
+    path: 'issue/:id',
+    component: DetailComponent
+  },
+  {
+    path: 'issues/new',
+    component: NewIssueComponent
+  },
+  {
+    path: 'issues',
+    component: IssueListComponent
+  }
+];
